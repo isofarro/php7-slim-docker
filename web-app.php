25980cf5b61889e11dfe3834d9222d0a3ff255f9
Mike Davies <isofarro2@gmail.com>
2019-12-25 12:07:38 +0000

    clean up web-app.php.

--- web-app.php
+++ web-app.php
@@ -1,22 +1,20 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
 use Slim\Views\TwigMiddleware;
 
 use YourNamespace\App as TheApp;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+// Create instance of our app, and get its container
 $theApp = TheApp::create();
 $container = $theApp->getContainer();
 
+// Create Slim app
 AppFactory::setContainer($container);
 $app = AppFactory::create();
-
 
 // Set Twig to render view layer
 $container->set('view', function ($container) {
@@ -24,12 +22,12 @@ $container->set('view', function ($container) {
     return $view;
 });
 
-// Add Twig-Middleware
+// Add Twig-Middleware - url_for() on templates
 $app->add(TwigMiddleware::createFromContainer($app));
 
 
 
-// Service-based endpoints
+// Service-based endpoints -- add more here
 require __DIR__ . '/var/routes/hello.php';
 
 
