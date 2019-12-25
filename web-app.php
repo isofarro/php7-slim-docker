35f692175bac6b3edb832601ed2ddd19fd781eaa
Mike Davies <isofarro2@gmail.com>
2019-12-25 11:47:28 +0000

    update web-app to support Twig, containers and an App instance.

--- web-app.php
+++ web-app.php
@@ -1,15 +1,36 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+use Slim\Views\TwigMiddleware;
+
+use YourNamespace\App as TheApp;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$theApp = TheApp::create();
+$container = $theApp->getContainer();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$app->get('/hello', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+
+// Set Twig to render view layer
+$container->set('view', function ($container) {
+    $view = Twig::create('var/views', ['cache' => false /* 'tmp/cache/views' */]);
+    return $view;
 });
+
+// Add Twig-Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+
+
+// Service-based endpoints
+require __DIR__ . '/var/routes/hello.php';
+
 
 $app->run();
